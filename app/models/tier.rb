5a3420fdfa23b5ce4cc7c12d51591920506df515
ts-miller <tylerstephen11@gmail.com>
2020-11-06 09:04:02 -0500

    added scoped validation to tiers

--- app/models/tier.rb
+++ app/models/tier.rb
@@ -1,4 +1,6 @@
 class Tier < ApplicationRecord
     belongs_to :creator
     validate :title, :description, :amount, presence: true
+    validate :title, uniqueness: { scope: :creator_id, message: 'title already taken' }
+    validate :amount, uniqueness: { scope: :creator_id, :message: 'tier amount already taken' }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_04_180527) do
+ActiveRecord::Schema.define(version: 2020_11_05_223120) do
 
   create_table "creators", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 2020_11_04_180527) do
     t.integer "user_id"
     t.integer "creator_id"
     t.decimal "amount"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tiers", force: :cascade do |t|
+    t.string "title"
+    t.integer "amount"
+    t.string "description"
+    t.integer "creator_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
